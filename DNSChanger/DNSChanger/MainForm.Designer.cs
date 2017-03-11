28e4b249af66347d44450e5798cb9ad1657ab017
AlexKozlovsci <kozlovscialecsei@mail.ru>
2017-03-11 17:30:28 +0300

    end program

--- DNSChanger/DNSChanger/MainForm.Designer.cs
+++ DNSChanger/DNSChanger/MainForm.Designer.cs
@@ -30,14 +30,17 @@
         {
             this.btnSet = new System.Windows.Forms.Button();
             this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.cmbbxNetworks = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // btnSet
             // 
+            this.btnSet.Enabled = false;
             this.btnSet.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btnSet.Location = new System.Drawing.Point(12, 12);
+            this.btnSet.Location = new System.Drawing.Point(152, 70);
             this.btnSet.Name = "btnSet";
-            this.btnSet.Size = new System.Drawing.Size(125, 73);
+            this.btnSet.Size = new System.Drawing.Size(125, 59);
             this.btnSet.TabIndex = 0;
             this.btnSet.Text = "DNS set";
             this.btnSet.UseVisualStyleBackColor = true;
@@ -45,20 +48,44 @@
             // 
             // btnDelete
             // 
+            this.btnDelete.Enabled = false;
             this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btnDelete.Location = new System.Drawing.Point(155, 12);
+            this.btnDelete.Location = new System.Drawing.Point(294, 70);
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(117, 73);
+            this.btnDelete.Size = new System.Drawing.Size(117, 59);
             this.btnDelete.TabIndex = 1;
             this.btnDelete.Text = "DNS delete";
             this.btnDelete.UseVisualStyleBackColor = true;
             this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnSearch.Location = new System.Drawing.Point(12, 70);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(125, 59);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Network search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // cmbbxNetworks
+            // 
+            this.cmbbxNetworks.Enabled = false;
+            this.cmbbxNetworks.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cmbbxNetworks.FormattingEnabled = true;
+            this.cmbbxNetworks.Location = new System.Drawing.Point(12, 25);
+            this.cmbbxNetworks.Name = "cmbbxNetworks";
+            this.cmbbxNetworks.Size = new System.Drawing.Size(399, 28);
+            this.cmbbxNetworks.TabIndex = 3;
+            // 
             // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 97);
+            this.ClientSize = new System.Drawing.Size(423, 146);
+            this.Controls.Add(this.cmbbxNetworks);
+            this.Controls.Add(this.btnSearch);
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.btnSet);
             this.Name = "mainForm";
@@ -71,6 +98,8 @@
 
         private System.Windows.Forms.Button btnSet;
         private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ComboBox cmbbxNetworks;
     }
 }
 
